39d8b0195bbebc324cd425f264a70e4b50a30c01
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2024-05-12 16:18:27 +0800

    made tests for imgsz=640

--- tools/inferenceTest.py
+++ tools/inferenceTest.py
@@ -1,10 +1,10 @@
 from ultralytics import YOLO
 
 # Load a model, use htop to analyze hardware performancex
-model = YOLO('models/best.onnx')  # load model here
+model = YOLO('../models_640/best_ncnn_model/')  # load model here
 
 # Run batched inference on a list of images
-results = model('rtsp://admin:Angelorafael69@192.168.1.99:554/Streaming/channels/101/',imgsz=800, vid_stride=1, agnostic_nms=True, max_det=100, iou=0.8, stream=True)  # return a generator of Results objects
+results = model('rtsp://admin:Angelorafael69@192.168.1.99:554/Streaming/channels/101/',imgsz=640, half=True, vid_stride=15, agnostic_nms=True, max_det=20, iou=0.8, stream=True)  # return a generator of Results objects
 
 # Process results generator
 for result in results:
